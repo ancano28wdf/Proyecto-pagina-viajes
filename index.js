fb117987d9d5af23f89872f8193fc71e8affcea8
ancano28wdf <angelanaomicano@gmail.com>
2024-06-24 21:48:20 -0500

    se revisaron los codigos

--- index.js
+++ index.js
@@ -1,50 +1,66 @@
 //Evento para crear un nuevo viaje
-document.getElementById(“formulario”).addEventListener(“submit, crear)
+document.getElementById("formulario").addEventListener("submit", crear)
 
 //Función crear
 function crear(){
-     Aeropuerto = document.getElementById(“aeropuerto”).value
-     strip-start = document.getElementById(“start”).value
-     tipo de viaje = document.getElementById(“tipo de viaje”).value
-     pago = document.getElementById(“pago”).value
+     Aeropuerto=document.getElementById("aeropuerto").value
+     strip-start=document.getElementById("start").value
+     tipodeviaje=document.getElementById("tipo de viaje").value
+     pago=document.getElementById("pago").value
 }
 let viaje = {
-     aeropuesrto,
+     aeropuerto,
      fecha,
      tipo de viaje,
-     pago
+     pago,
 }
-if(localStoreg.getItem(“viajes”) === null) {
+if(localStorage.getItem("viajes") === null) {
      let viajes = []
       viajes.push(viaje)
-      localStoreg.insertItem(“viajes”, JSON.stringify(viajes))
+      localStorage.sertItem("viajes", JSON.stringify(viajes))
+}
 else{
      let viajes = JSON.parse(localStorage.getItem("viajes"))
       viajes.push(viajes)
-      localStoreg.setItem(“viajes”, JSON.stringify(viajes))
+      localStorage.setItem("viajes", JSON.stringify(viajes))
 }
-
-document.getElementById(“formulario”).reset();
-console.log(“Viaje gusrdado correctamente”)
+leer ();{
+document.getElementById("formulario").reset();
+console.log("Viaje guardado correctamente")
 e.preventDefault()
 }
 
 //Función “leer”
 
 function leer(){
-          let viajes = JSON.parse(localStoreg.getItem(“viajes”));
-          document.getElementById(“tbody”).innerHTML = “”
+          let viajes = JSON.parse(localStorage.getItem("viajes"));
+          document.getElementById("tbody").innerHTML = ""
          for(let i=0; i < viajes.length; i++) {
-                      let aeropuesrto = viajes[i].nombre
-                      let fecha = viajes[i].destino
-                      let tipo de viaje = viajes[i].no. personas
-                      let tarjeta = viajes[i].tarjeta
+                      let aeropuerto = viajes[i].aeropuerto
+                      let start = viajes[i].start
+                      let tipodeviaje = viajes[i].tipodeviaje
+                      let pago = viajes[i].pago
 
-                      document.getElementById(“tbody”).innerHTML +=
-                      ‘<tr>
-                             <td>${aeropuesrto}</td>
-                             <td>${fecha}</td>
-                             <td>${tipo de viaje}</td>
-                             <td>${tarjeta}</td>
-´
+                      document.getElementById("tbody").innerHTML +=
+                      ´<tr>
+                             <td>${aeropuerto}</td>
+                             <td>${start}</td>
+                             <td>${tipodeviaje}</td>
+                             <td>${pago}</td>
+                             <td><buttom onclick="editar(´${titulo})" class="btn btn-sucess">Editar</buttom></td>
+                       ´</tr> 
+
+          }
 }
+
+// Funcion editar
+function editar(titulo){
+     let viajes = JSON.parse(localStorage.getItem("viajes"));
+     for (let i=0; i < viajes.length; i++){
+         if(viajes [i].titulo){
+          document.getElementById("body").innerHTML=
+           
+          }
+     }
+}
+leer()
