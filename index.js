e297078ad34ee92c049e9ead3fb8640dd399114f
ancano28wdf <angelanaomicano@gmail.com>
2024-06-27 20:16:20 -0500

    cambio de imagenes y agregue funciones

--- index.js
+++ index.js
@@ -47,8 +47,10 @@ function leer(){
                              <td>${start}</td>
                              <td>${tipodeviaje}</td>
                              <td>${pago}</td>
-                             <td><buttom onclick="editar(´${titulo})" class="btn btn-sucess">Editar</buttom></td>
-                       ´</tr> 
+                             <td><buttom onclick="editar(´${titulo}')" class="btn btn-sucess">Editar</buttom></td>
+                       </tr>
+                      ´
+                        
 
           }
 }
@@ -56,11 +58,118 @@ function leer(){
 // Funcion editar
 function editar(titulo){
      let viajes = JSON.parse(localStorage.getItem("viajes"));
-     for (let i=0; i < viajes.length; i++){
-         if(viajes [i].titulo){
-          document.getElementById("body").innerHTML=
-           
+     for (let i=0; i <viajes.length; i++){
+         if(viajes[i].titulo === titulo){
+          document.getElementById("body").innerHTML = ´               
+                            <form  class="body">
+                            <section class="form_container"> 
+                            <form class="form">
+                            <input  type="text"  class="form_imput" name="Aeropueto"  id="newaeropueto" required autocomplete="off" placeholder="${viajes[i].aeropuerto}">
+
+                            <label for="start"></label>
+                            <input type="date" id="newstart" name="trip-start" value="2024-06-29" min="2024-01-01" max="2025-12-31">
+
+                            <label for="tipo deviaje">
+                               <span>Elije nuevo Tipo de viaje:</span>
+                            </label>
+                            <select id="newtipo de viaje" name="tipo de viaje">
+                                <option value="combinado">Combinado</option>
+                                <option value="individual">Individual</option>
+                            </select>
+
+                            <label for="pago">
+                               <span>Pago:</span>
+                            </label>
+                            <select id="newpago" name="pago">
+                                 <option value="visa">VISA</option>
+                                 <option value="mc">Master Card</option>
+                                 <option value="amex">American Express</option>
+                            </select>
+                            <buttom class="btn btn-success" onclick="Actualizar(´${i}´)">Actualizar</buttom>
+                            <buttom class="btn btn-primary" onclick="VistaPrincipal()">Cancelar</buttom>
+   
+         
+                  ´
           }
      }
 }
+
+//Funcion Actualizar
+function actualizar(i) {
+     let viajes = JSON.parse(localStorage.getItem("viajes"));
+     viajes[i].aeropuerto = document.getElementById("newaeropuerto").value;
+     viajes[i].start = document.getElementById("newstart").value;
+     viajes[i].ipo de viaje = document.getElementById("newtipo de viaje").value;
+     viajes[i].pago = document.getElementById("newpago").value;
+
+          localStorage.setItem("viajes", JSON.stringify(viajes));
+          vistaPrincipal()
+}
+
+//Funcion eliminar
+function eliminar(titulo) {
+     let viajes = JSON.parse(localStorage.getItem("viajes"));
+     for(let i=0; i<viajes.length; i){
+          if(viajes[i].titulo === titulo)
+               viajes.splice(i,1);
+     }
+}
+
+localStorage.setItem("viajes", JSON.stringify(viajes));
+leer();
+
+//Funcion para mostrar la interfaz principal 
+function vistaPrincipal() {
+     document.getElementById("body").innerHTML = ´      <form id="formulario" class="body">
+     <section class="form_container"> 
+      <form class="form">
+        <input  type="text"  class="form_imput" name="Aeropueto"  id="aeropueto" required autocomplete="off" placeholder="Aeropuerto">
+       
+        <label for="start"></label>
+        <input type="date" id="start" name="trip-start" value="2024-06-29" min="2024-01-01" max="2025-12-31">
+
+        <label for="tipo deviaje">
+          <span>Tipo de viaje:</span>
+        </label>
+        <select id="tipo de viaje" name="tipo de viaje">
+         <option value="combinado">Combinado</option>
+         <option value="individual">Individual</option>
+        </select>
+
+        <label for="pago">
+          <span>Pago:</span>
+        </label>
+        <select id="pago" name="pago">
+          <option value="visa">VISA</option>
+          <option value="mc">Master Card</option>
+          <option value="amex">American Express</option>
+        </select>
+  
+        <input type="submit" class="botton" value="Reservar">
+      </form>
+     </section>
+     </form> 
+    </div>
+ <div class="col-md-6">
+     <table class="table">
+        <thread>
+          <tr class="col">
+            <th scope="col">Aeropuerto</th>
+            <th scope="col">Fecha de viaje</th>
+            <th scope="col">Viaje</th>
+            <th scope="col">Pago</th>
+         </tr>
+      </thread>
+      <tbody id="tbody">
+         <tr>
+             <td>-</td>
+             <td>-</td>
+             <td>-</td>
+             <td>-</td>
+         </tr>
+      </tbody>
+     </table>
+     ´
+}
+
 leer()
